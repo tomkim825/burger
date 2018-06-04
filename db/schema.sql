0f0845db5eabf1017bc9886af2bf272d96e27c23
tomkim825 <tk825@hotmail.com>
2018-06-04 11:43:53 -0700

    added more directories

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+id INT NOT NULL AUTO_INCREMENT,
+burger_name VARCHAR(100) NULL,
+devoured BOOLEAN NOT NULL DEFAULT 0,
+PRIMARY KEY (id)
+);
+
+SELECT * FROM burgers;
---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES
+('Bacon cheeseburger', false),
+('California cheeseburger', false),
+('Double Double', false);
+
+SELECT * FROM burgers;
