299b6d56fe6e4eed06d7be5c6b4b747e1bc72282
tomkim825 <tk825@hotmail.com>
2018-06-04 22:19:53 -0700

    got express static part working!

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
-app.use(express.static('public/assets/img/')); 
+app.use(express.static('public/')); 
 
 // ==============================================================================
 // Handlebars CONFIGURATION
